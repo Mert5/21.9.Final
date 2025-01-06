16c4dfddaaa749d666fdf3dbfeee1981f770efe3
Mert5 <39389585+Mert5@users.noreply.github.com>
2025-01-06 23:59:47 +0300

    Add files via upload

---
+++ 21.9.Final/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Mert Cullukcu"<< std::endl;
+    std::cout << "Mert Cullukcu"<< std::endl;
+    std::cout << "Mert Cullukcu"<< std::endl;
+    std::cout << "Mert Cullukcu"<< std::endl;
+    std::cout << "Mert Cullukcu"<< std::endl;
+    std::cout << "Mert Cullukcu"<< std::endl;
+    std::cout << "Mert Cullukcu"<< std::endl;
+    std::cout << "Mert Cullukcu"<< std::endl;
+    std::cout << "Mert Cullukcu"<< std::endl;
+    std::cout << "Mert Cullukcu"<< std::endl;
+    std::cout << "Welcome"<< std::endl;
+    
+    return 0;
+}
