4816e9476b5b1ccf58317261523e4684f8d40f44
Mert5 <39389585+Mert5@users.noreply.github.com>
2025-01-07 16:09:22 +0300

    Update main.cpp

--- 21.9.Final/main.cpp
+++ 21.9.Final/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 
+// References => https://www.youtube.com/watch?v=8jLOx1hD3_o
+//            => https://github.com/rutura/The-C-20-Masterclass-Source-Code/blob/main/37.Polymorphism/37.10Final
+
+//Intersting fact #1
+/*
+class Plane final{
+    Plane()=default;
+};
+
+//This will trigger a compiler error
+class FigherJet : public Plane{
+    
+};
+*/
+
 int main(){
 
-    std::cout << "Mert Cullukcu"<< std::endl;
-    std::cout << "Mert Cullukcu"<< std::endl;
-    std::cout << "Mert Cullukcu"<< std::endl;
-    std::cout << "Mert Cullukcu"<< std::endl;
-    std::cout << "Mert Cullukcu"<< std::endl;
-    std::cout << "Mert Cullukcu"<< std::endl;
-    std::cout << "Mert Cullukcu"<< std::endl;
-    std::cout << "Mert Cullukcu"<< std::endl;
-    std::cout << "Mert Cullukcu"<< std::endl;
-    std::cout << "Mert Cullukcu"<< std::endl;
-    std::cout << "Welcome"<< std::endl;
-    
+    std::cout << "Hello" << std::endl;
     return 0;
 }
